2d2ee71a1d8d7e579d43c1e294686553c84e1461
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2026-06-08 23:36:30 -0600

    Delete innecesary prefixes

--- api/portfolio/routers/__init__.py
+++ api/portfolio/routers/__init__.py
@@ -8,8 +8,8 @@ from .certificate import router as certificate_router
 
 portfolio_router = APIRouter()
 
-portfolio_router.include_router(tag_router, prefix="/tags", tags=["Portfolio: Tags"])
-portfolio_router.include_router(project_router, prefix="/project", tags=["Portfolio: Project"])
-portfolio_router.include_router(project_translation_router, prefix="/project_translation", tags=["Portfolio: Project Translation"])
-portfolio_router.include_router(academy_router, prefix="/academy", tags=["Portfolio: Academy"])
-portfolio_router.include_router(certificate_router, prefix="/certificate", tags=["Portfolio: Certificate"])
+portfolio_router.include_router(tag_router, tags=["Portfolio: Tags"])
+portfolio_router.include_router(project_router, tags=["Portfolio: Project"])
+portfolio_router.include_router(project_translation_router, tags=["Portfolio: Project Translation"])
+portfolio_router.include_router(academy_router, tags=["Portfolio: Academy"])
+portfolio_router.include_router(certificate_router, tags=["Portfolio: Certificate"])
