ebfe3f774c897f5fdbefc83f40e352460ec001c1
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2026-06-23 00:25:26 -0600

    Hotfix crash due projectpy

--- alembic/env.py
+++ alembic/env.py
@@ -1,14 +1,13 @@
 from logging.config import fileConfig
 from os import getenv
 
+from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
-
 from api.database import SQLModel
+
 #
-from api.portfolio.models import ProjectType, DifficultyLevel, Category, Language, Project, ProjectTranslation, Certification, Course
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -74,9 +73,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
